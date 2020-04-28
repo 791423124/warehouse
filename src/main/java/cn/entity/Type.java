7921b37116efba365eab7d80576dd3a2dc997b1b
791423124 <791423124@qq.com>
2020-04-28 19:49:07 +0800

    框架

---
+++ src/main/java/cn/entity/Type.java
@@ -0,0 +1,14 @@
+package cn.entity;
+
+import java.util.Date;
+
+public class Type {
+    private Integer id;
+    private String typeName;
+    private Integer parentId;
+    private Date createDate;
+    private Integer createId;
+    private Date modifyDate;
+    private Integer modifyId;
+    private Integer isDel;
+}
